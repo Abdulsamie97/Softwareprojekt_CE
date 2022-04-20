a5da063ecc26656baeef08974b1074c995d3495e
Thomas Baar <thomas.baar@htw-berlin.de>
2022-04-20 08:47:14 +0200

    minor

--- pokemon-app/src/App.vue
+++ pokemon-app/src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-  </nav>
+  <div class="p-14">
+    <router-link class="flex justify-center text-4xl text-yellow-700" to="/">Home</router-link>
+  </div>
   <router-view/>
 </template>
 
