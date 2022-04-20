fa4ddc58d1b00261968e020ee912b7c2cbb3075b
Thomas Baar <thomas.baar@htw-berlin.de>
2022-04-20 10:37:38 +0200

    new branch

--- pokemon-app/src/views/HomeView.vue
+++ pokemon-app/src/views/HomeView.vue
@@ -1,21 +1,35 @@
 <template>
   <div class="home">
     <h3>Hello World</h3>
+    {{pokemons}}
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import {reactive, toRefs} from "vue";
 
 export default {
   name: 'HomeView',
     setup(){
 
+      const state=reactive(
+        {
+          pokemons: [],
+          urlIdLookup: {}
+        }
+      )
+
+
     fetch("https://pokeapi.co/api/v2/pokemon?offset=0")
     .then((res) => res.json())
     .then((data) => {
       console.log(data)
+      state.pokemons = data.results;
+      state.urlIdLookup = data.results.reduce((acc, cur, idx)  => 
+        acc={...acc, [cur.name]:idx+1 }
+        ,{})
     } )
+    return {...toRefs(state)}
   }
 }
 </script>
